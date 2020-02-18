10532662d2753d343efd975e93605e8eb0c738fe
GBTC440005UR <GBTC440005UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-18 14:18:16 -0500

    first commit

---
+++ src/main/java/com/max/springboot_pagefragments/SpringbootPagefragmentsApplication.java
@@ -0,0 +1,13 @@
+package com.max.springboot_pagefragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPagefragmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPagefragmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/max/springboot_pagefragments/SpringbootPagefragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.springboot_pagefragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPagefragmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
